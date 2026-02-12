9b0f22dc89acd0d2d097faf4c5576fe01011bac2
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-12 22:51:08 +0530

    Add kilometers to miles conversion program

---
+++ Assignment4.py
@@ -0,0 +1,4 @@
+#Problem statement - Create a program to convert kilometers to miles
+km = float(input("Enter distance in kilometers: "))
+miles = km * 0.621371
+print(f"{km} kilometers is equal to {miles} miles.")
