5891d6ba02b02f9cd5bbeb0a34144d1f9df645b0
kunal-kalamkar77 <kunal.1251100220@vit.edu>
2026-02-13 22:51:41 +0530

    Add program to check if a number is positive, negative, or zero

---
+++ Assignment7.py
@@ -0,0 +1,8 @@
+#Problem statement -  Create a program to check if the given number is positive, negative or zero.
+number = float(input("Enter a number: "))
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
